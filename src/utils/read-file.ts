83d382ae771b9238defbc128d90351d29f9cb6c0
safdar-azeem <safdarazeem00@gmail.com>
2021-09-16 03:55:00 +0000

    Add read-file.ts

---
+++ src/utils/read-file.ts
@@ -0,0 +1,8 @@
+export const readFile = (file: File) => {
+  const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = reject
+    reader.readAsDataURL(file)
+  })
+}
