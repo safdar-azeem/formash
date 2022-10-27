e314c06990ae2bba84a9d9c687f30bb4d15257da
safdar-azeem <safdarazeem00@gmail.com>
2022-10-27 06:29:05 +0500

    Refactor and Add More types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type Type =
+export type IType =
   | 'text'
   | 'number'
   | 'email'
@@ -22,30 +22,67 @@ export type Type =
   | 'image'
   | 'reset'
   | 'submit'
-  | 'button';
+  | 'button'
 
 export interface IFormSchema {
-  type: Type;
-  name: string;
-  placeholder: string;
-  label: string;
-  required: boolean;
-  options?: IFormOption[];
-  accept?: string;
-  value?: any;
-  error: string;
+  type: IType
+  name: string
+  id?: string
+  required?: boolean
+  placeholder?: string
+  className?: string
+  label?: string
+  labelClassName?: string
+  text?: string
+  textClassName?: string
+  icon?: string
+  iconClassName?: string
+  options?: IFormOption[]
+  accept?: string
+  value?: any
+  error?: string
+  readOnly?: boolean
+  disabled?: boolean
+  autoFocus?: boolean
+  multiple?: boolean
+  pattern?: string
+  autoComplete?: 'on' | 'off'
+  spellCheck?: boolean
+  step?: number
+  formTarget?: '_self' | '_blank' | '_parent' | '_top' | 'framename'
+  alt?: string
+  validation?: (value: any) => string
+  trim?: boolean
+  /* minLength or maxLength is used to validate the length of the input value 
+	either as a number or as an array of [number, string] 
+	where the string is the error message 
+	*/
+  minLength?: number | [number, string]
+  maxLength?: number | [number, string]
+  /* The enum property is used to define a list of possible values or a single value for the input 
+	if the value of the input is not matched with enum, the error message will be displayed
+	*/
+  enum?: IEnum
 }
+
+export type IEnum = Array<string | number> | string | number
 
 export interface IFormOption {
-  label: string;
-  value: string;
-  name?: string;
-  checked?: boolean;
+  name?: string
+  value: string
+  label: string
+  text?: string
+  icon?: string
+  checked?: boolean
+  disabled?: boolean
+  className?: string
+  style?: React.CSSProperties
 }
 
-export interface IFormState {
-  [key: string]: {
-    value: any;
-    error: string;
-  };
+export interface IFormValues {
+  [key: string]: any
+}
+
+export interface IFormErrors {
+  [key: string]: string
 }
